f63df9ec36cd8ddf831110812899a39ed9a81159
CDavies <41549803+cjjdavies@users.noreply.github.com>
2021-04-04 16:45:59 +0100

    Add files via upload
    FIrst attempt at creating a simple function that performs division on two integers and returns the remainder

---
+++ first_function.py
@@ -0,0 +1,11 @@
+def remainder(num1, num2):
+    """
+    Exercise to create a function that returns the remainder of division
+    :param num1: integer
+    :param num2: integer
+    :return: the remainder, using modulus operator
+    """
+    return num1 % num2
+
+result = remainder(10, 3)
+print(result)
